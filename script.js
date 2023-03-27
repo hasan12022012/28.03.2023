39d4cc2414873872c2576baa5ae4a6c36a40e7b2
Hasan Huseynli <hasansh@code.edu.az>
2023-03-28 00:01:22 +0400

    taskAdded

---
+++ script.js
@@ -0,0 +1,11 @@
+let x = prompt('Ədəd daxil edin:');
+
+if (x > 0) {
+    console.log('Müsbətdir');
+}
+else if (x == 0 && x=='string') {
+    console.log('Yendiən daxil edin:')
+}
+else {
+    console.log('Mənfidir')
+}
